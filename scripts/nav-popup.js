17104361c5f52613141902f1418cc4cdea136d50
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-08-05 09:41:05 +0300

    fix: fix scroll bug in popup for firefox

--- scripts/nav-popup.js
+++ scripts/nav-popup.js
@@ -2,13 +2,31 @@ const popup = document.querySelector('.popup');
 const closePopupButton = popup.querySelector('.popup__close-btn');
 const openPopupButton = document.querySelector('.header__navigation-mobile-icon');
 const body = document.querySelector('.page');
+let winX = null;
+let winY = null;
 
 openPopupButton.addEventListener('click', () => {
   popup.classList.toggle('popup_opened');
-  body.classList.toggle('page_state_hidden');
+  disableWindowScroll();
 });
 
 closePopupButton.addEventListener('click', () => {
   popup.classList.toggle('popup_opened');
-  body.classList.toggle('page_state_hidden');
+  enableWindowScroll();
 });
+
+window.addEventListener('scroll', function () {
+  if (winX !== null && winY !== null) {
+    window.scrollTo(winX, winY);
+  }
+});
+
+function disableWindowScroll() {
+  winX = window.pageXOffset;
+  winY = window.pageYOffset;
+}
+
+function enableWindowScroll() {
+  winX = null;
+  winY = null;
+}
