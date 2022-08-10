a155b87b764eed83a5b184588fd0f00603987e7d
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-08-10 13:11:25 +0300

    feat: add four columns in main page

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,5 +1,29 @@
 const initialPosts = [
   {
+    href: 'http://xn--80ajoghfjyj0a.xn--p1ai/%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D0%B0-%D0%B7%D0%B0%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%B0-%D1%82%D0%B0%D0%B9%D0%BD%D1%8B%D0%B5-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%D1%8B-%D0%B2-%D1%84%D0%B8/',
+    src: './images/shegalow-on-air.jpeg',
+    time: '06 мая 2022',
+    text: 'Практика заговора, тайные символы в фильмах, оккультные игры элиты. Вадим Шегалов',
+  },
+  {
+    href: 'http://xn--80ajoghfjyj0a.xn--p1ai/%D0%BA%D0%BE%D1%80%D0%BD%D0%B8-%D1%83%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D1%81%D0%BA%D0%BE%D0%B3%D0%BE-%D0%BA%D1%80%D0%B8%D0%B7%D0%B8%D1%81%D0%B0-%D0%B5%D0%B2%D0%B3%D0%B5%D0%BD%D0%B8%D0%B9-%D1%81%D0%BF/',
+    src: './images/spicyn-on-air.jpeg',
+    time: '11 мая 2022',
+    text: 'Корни украинского кризиса. Евгений Спицын',
+  },
+  {
+    href: 'http://xn--80ajoghfjyj0a.xn--p1ai/%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B5-%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-%D0%BF%D0%B5%D1%80%D0%B2%D1%8B%D0%B5-%D0%B8%D0%B3%D0%BE%D1%80%D1%8C-%D1%81%D0%BE%D0%BB%D0%BE%D0%BD%D1%8C%D0%BA%D0%BE/',
+    src: './images/solonko-kosmos.jpeg',
+    time: '14 мая 2022',
+    text: 'Русские всегда первые. Игорь Солонько',
+  },
+  {
+    href: 'http://xn--80ajoghfjyj0a.xn--p1ai/%D0%B8%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7-%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8-%D1%8E%D1%80%D0%B8%D0%B9-%D0%B6%D1%83%D0%BA%D0%BE%D0%B2/',
+    src: './images/jykov-posol.jpeg',
+    time: '16 мая 2022',
+    text: 'Исторический анализ книги. Юрий Жуков',
+  },
+  {
     href: 'http://xn--80ajoghfjyj0a.xn--p1ai/%D0%BB%D0%B5%D1%82%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5-%D0%BE%D1%80%D1%83%D0%B6%D0%B8%D0%B5-%D0%B2%D0%BE%D0%B7%D0%BC%D0%B5%D0%B7%D0%B4%D0%B8%D1%8F-%D0%B2-%D0%B3%D0%B8%D0%B1%D1%80%D0%B8%D0%B4%D0%BD/',
     src: './images/efimov-streight.jpeg',
     time: '17 мая 2022',
